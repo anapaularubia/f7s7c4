db40c179b70042e2451bfb83b8cad1f171dedf92
Luis <bullon@hotmail.com>
2015-03-10 12:26:08 -0500

    2da etap de prueba<

--- Demo/src/DemoPrueba.java
+++ Demo/src/DemoPrueba.java
@@ -4,6 +4,12 @@ public class DemoPrueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("PRUEBA");
+		System.out.println("1ra ETAPA");
+		System.out.println("2da ETAPA");
+		System.out.println("3ra ETAPA");
+		System.out.println("4ta ETAPA");
+		System.out.println("5ta ETAPA");
+		
 	}
 
 }
