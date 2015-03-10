d069e224baf8c484266e8ff6afdcef70c3d989ea
lcamarena <lcamarena@TICEDS0005.migraciones.gob.pe>
2015-03-10 18:12:32 -0500

    KSAKFHSDHFHDSKFHDKSFHK

--- Demo/src/Persona.java
+++ Demo/src/Persona.java
@@ -4,6 +4,10 @@ public class Persona {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("demo");
+		System.out.println("demo1");
+		System.out.println("demo2");
+		System.out.println("demo3");
+		System.out.println("demo4");
 		
 	}
 
